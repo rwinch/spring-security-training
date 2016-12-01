c26ffb85011d91f0d1ae3eecb618b3d0d65a5966
Rob Winch <rwinch@gopivotal.com>
2016-11-30 21:45:03 -0800

    Saving

--- 02-hellosecurity/src/main/java/sample/SecurityConfig.java
+++ 02-hellosecurity/src/main/java/sample/SecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableWebSecurity
 public class SecurityConfig {
 
-
 	@Bean
 	public UserDetailsService userDetailsService() {
 		UserDetails user = User
